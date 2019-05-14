4df5c41f9e5ae06c7e0cdb3fb24faa140e84bac8
SambaLim <limsungho07@hanmail.net>
2019-05-14 23:13:05 +0900

    [Add] model Planet

--- workspace005/flutter_planets02/lib/ui/home/HomePageBody.dart
+++ workspace005/flutter_planets02/lib/ui/home/HomePageBody.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_planets02/ui//home/PlanetRow.dart';
+import 'package:flutter_planets02/model/Planets.dart';
 
 class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new PlanetRow();
+    return new PlanetRow(planets[0]);
   }
 }
--- workspace005/flutter_planets02/lib/ui/home/PlanetRow.dart
+++ workspace005/flutter_planets02/lib/ui/home/PlanetRow.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_planets02/model/Planets.dart';
 
 class PlanetRow extends StatelessWidget {
 
-  final planetThumbnail = Container(
-    margin: EdgeInsets.symmetric(
-      vertical: 16.0
-    ),
-    alignment: FractionalOffset.centerLeft,
-    child: new Image(
-      image: new AssetImage("assets/img/mars.png"),
-      height: 92.0,
-      width: 92.0
-    )
-  );
+  final Planet planet;
 
-  final planetCard = Container(
-    height: 124.0,
-    margin: EdgeInsets.only(left:46.0),
-    decoration: BoxDecoration(
-      color: Color(0xFF333366),
-      shape: BoxShape.rectangle,
-      borderRadius: BorderRadius.circular(8.0),
-      boxShadow: <BoxShadow>[
-        BoxShadow(
-          color: Colors.black12,
-          blurRadius: 10.0,
-          offset: Offset(0.0, 10.0),
-        )
-      ]
-    )
-  );
+  PlanetRow(this.planet);
 
   @override
   Widget build(BuildContext context) {
+
+    final planetThumbnail = Container(
+        margin: EdgeInsets.symmetric(
+            vertical: 16.0
+        ),
+        alignment: FractionalOffset.centerLeft,
+        child: new Image(
+            image: new AssetImage(planet.image),
+            height: 92.0,
+            width: 92.0
+        )
+    );
+
+    final planetCard = Container(
+        height: 124.0,
+        margin: EdgeInsets.only(left:46.0),
+        decoration: BoxDecoration(
+            color: Color(0xFF333366),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(8.0),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 10.0),
+              )
+            ]
+        )
+    );
+
     return new Container(
       margin: const EdgeInsets.symmetric(
         vertical: 16.0,
